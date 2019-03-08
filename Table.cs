2d0d1e6b57cb14db53bd03324d68a42890f37273
Alexandru Constantin <constantin.alex96@icloud.com>
2019-03-08 21:54:04 +0200

    Add files via upload

---
+++ Table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IBAN_Manager_API
+{
+    class Table
+{
+    public static void LoadTable(Dictionary<string, string> table)
+    {
+        try
+        {
+            var lines = File.ReadAllLines("table.ini");
+
+            foreach (var line in lines)
+            {
+                string[] values = line.Split(',');
+
+                table.Add(values[0], values[1]);
+
+            }
+        }
+        catch { }
+    }
+}
+}
